09d4e8131c6f74e658a5df22493d61873972b4d1
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-08-20 22:06:09 +0530

    Added Stress Test for LL

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,65 @@
 #include <iostream>
-#include <sstream>
+#include <vector>
+#include <numeric>
+#include <chrono>
 #include "lib/LinkedList/LinkedList.h"
 #include "lib/LinkedList/LinkedList.cpp"
 
+
 using namespace std;
 
-int main() {
+const long SIZE = 100000;
+const long PROBE_COUNT = 10000;
+
+double average(vector<long> &timeList) {
+    auto sum = accumulate(timeList.begin(), timeList.end(), (long long) 0);
+    return ((double) sum) / ((double) timeList.size());
+}
+
+void LLStressTest() {
     LinkedList<long> ll;
-    auto nodePtr = ll.find(1);
+    vector<long> generatedData;
 
+    vector<long> timeList;
 
-    cout << (nodePtr == nullptr ? "Nullptr" : "Not");
+    for (long i = 0; i < SIZE; i++) {
+        cout << "\rAdding Elements: " << i << "/" << SIZE;
+        auto randNum = random();
+
+        ll.push_back(randNum);
+        generatedData.push_back(randNum);
+    }
+    cout << "\nElements Added\n";
+
+    for (long j = 0; j < PROBE_COUNT; j++) {
+        long randomIdx = random() % SIZE;
+
+        cout << "\r" << j << "/" << PROBE_COUNT << ": Finding Element with Idx " << randomIdx << ": ";
+        const auto startTime = clock();
+        auto nodePtr = ll.find(generatedData[randomIdx]);
+        const auto endTime = clock();
+
+        if (nodePtr == nullptr) {
+            cout << "Error: nodePtr is null";
+            throw;
+        }
+        if (nodePtr->getData() != generatedData[randomIdx]) {
+            cout << "Error: Data does not match" << endl;
+            cout << "nodePtr->getData(): " << nodePtr->getData() << endl;
+            cout << "generatedData[randomIdx]: " << generatedData[randomIdx] << endl;
+        }
+
+        const auto timeTaken = endTime - startTime;
+        cout << timeTaken << " ms";
+
+        timeList.push_back(timeTaken);
+    }
+
+    auto avgTimeTaken = average(timeList);
+
+    cout << "\nAverage Time Taken for a LL with " << SIZE << " elements: " << avgTimeTaken << " ms\n";
+}
+
+int main() {
+    LLStressTest();
 }
